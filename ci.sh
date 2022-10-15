90a04fdff2566684605818403b230657ab47fde7
Verter Ace <103939173+Laif7joo@users.noreply.github.com>
2022-10-15 19:51:57 +0300

    cd subj && git describe

--- ci.sh
+++ ci.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+function failure() {
+	echo "Oops, $* failed ;(" >&2
+	exit 2
+}
+
+export ci_script_recursion="$((++ci_script_recursion))"
+if [ $ci_script_recursion -gt 3 ]; then
+     failure "WTF: ci_script_recursion = $ci_script_recursion ?"
+fi
+
 IFS='|' read -r -a config_args <<< "${1:-}"
 IFS='|' read -r -a build_args <<< "${2:-}"
 IFS='|' read -r -a test_args <<< "${3:-}"
@@ -35,15 +45,15 @@ function default_build {
 		echo "NINJA: $(which ninja 2>/dev/null) => $(ninja --version | head -1)"
 		cmake_use_ninja="-G Ninja"
 	fi
-	cmake ${cmake_use_ninja} "${config_args[@]+"${config_args[@]}"}" ../subj && cmake --build . "${build_args[@]+"${build_args[@]}"}"
+	cmake ${cmake_use_ninja} "${config_args[@]+"${config_args[@]}"}" .. && cmake --build . "${build_args[@]+"${build_args[@]}"}"
 }
 
 function default_ci {
 	provide_toolchain
-	if [ -e subj/CMakeLists.txt -a $CMAKE_VERSION -ge 30802 ]; then
+	if [ -e CMakeLists.txt -a $CMAKE_VERSION -ge 30802 ]; then
 		mkdir @build && cd @build && default_build && default_test && echo "Done (cmake)"
-	elif [ -e subj/GNUmakefile -o -e subj/Makefile -o -e subj/makefile ]; then
-		make -C subj -j2 all && make -C subj test && echo "Done (make)"
+	elif [ -e GNUmakefile -o -e Makefile -o -e makefile ]; then
+		make -j2 all && make test && echo "Done (make)"
 	else
 		echo "Skipped since CMAKE_VERSION ($CMAKE_VERSION) < 3.8.2 and no Makefile"
 	fi
@@ -53,20 +63,22 @@ git clean -x -f -d || echo "ignore 'git clean' error"
 git config --global submodule.fetchJobs 2
 
 if [ -f url ]; then
-	git clone -q --single-branch --recurse-submodules $(cat url) subj
+	git clone -q --single-branch --recurse-submodules $(cat url) subj && cd subj || failure git-clone
 else
-	git submodule sync --recursive
-	git submodule update --init --recursive
-	#git submodule foreach --recursive 'git fetch $(test -f .git/shallow && echo --unshallow) --tags --prune --force'
-	git submodule foreach --recursive git fetch --update-shallow --tags --prune --force
+	git submodule sync --recursive || failure git-submodule-sync
+	git submodule update --init --recursive || failure git-submodule-update
+	#git submodule foreach --recursive 'git fetch $(test -f .git/shallow && echo --unshallow) --tags --prune --force' || failure git-submodule-fetch
+	git submodule foreach --recursive git fetch --update-shallow --tags --prune --force || failure git-submodule-fetch
 fi
 
-if [ -x ./subj/test/ci.sh ]; then
-	ci_script=subj/test/ci.sh
-elif [ -x ./subj/ci.sh ]; then
-	ci_script=subj/ci.sh
+git describe --tags || git show --oneline -s
+
+if [ -x test/ci.sh ]; then
+	ci_script=./test/ci.sh
+elif [ -x ci.sh ]; then
+	ci_script=./ci.sh
 else
 	ci_script=default_ci
 fi
 
-$ci_script
+$ci_script || failure $ci_script
